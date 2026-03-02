185df76cb011a4520b68c4c74288e6700fa6f11f
guguangyue <guguangyue@allcam.com.cn>
2026-03-02 14:39:32 +0800

    模糊，边缘检测

--- FrameBuffers/mainwindow.cpp
+++ FrameBuffers/mainwindow.cpp
@@ -11,6 +11,9 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->pushButton , &QPushButton::clicked , this, [=](){emit button_click(1);});
     connect(ui->pushButton_2 , &QPushButton::clicked , this, [=](){emit button_click(2);});
     connect(ui->pushButton_3 , &QPushButton::clicked , this, [=](){emit button_click(3);});
+    connect(ui->pushButton_4 , &QPushButton::clicked , this, [=](){emit button_click(4);});
+    connect(ui->pushButton_5 , &QPushButton::clicked , this, [=](){emit button_click(5);});
+    connect(ui->pushButton_6 , &QPushButton::clicked , this, [=](){emit button_click(6);});
 }
 
 MainWindow::~MainWindow()
--- FrameBuffers/quadFragment.glsl
+++ FrameBuffers/quadFragment.glsl
@@ -5,6 +5,8 @@ in vec2 texCoord;
 uniform sampler2D textureIn;
 uniform int renderFlag;
 
+const float offset = 1.0 / 300.0;
+
 void main()
 {
     if(renderFlag == 1)
@@ -15,5 +17,83 @@ void main()
         FragColor = texture(textureIn, texCoord);
         float average = (FragColor.r + FragColor.g + FragColor.b)/3.0;
         FragColor = vec4(average, average, average, 1.0);
+    }else if(renderFlag == 4){
+        vec2 offsets[9] = vec2[](
+            vec2(-offset, offset),
+            vec2(0.0, offset),
+            vec2(offset, offset),
+            vec2(-offset, 0.0),
+            vec2(0.0, 0.0),
+            vec2(offset, 0.0),
+            vec2(-offset, -offset),
+            vec2(0.0, -offset),
+            vec2(offset, -offset)
+        );
+        float kernel[9] = float[](
+            -1, -1, -1,
+            -1,  9, -1,
+            -1, -1, -1);
+
+        vec3 samples[9];
+        for(int i = 0; i < 9; i++){
+            samples[i] = texture(textureIn, texCoord + offsets[i]).rgb;
+        }
+        vec3 col = vec3(0.0);
+        for(int i = 0; i < 9; i++){
+            col += samples[i] * kernel[i];
+        }
+        FragColor = vec4(col, 1.0);
+    }else if(renderFlag == 5){
+        vec2 offsets[9] = vec2[](
+            vec2(-offset, offset),
+            vec2(0.0, offset),
+            vec2(offset, offset),
+            vec2(-offset, 0.0),
+            vec2(0.0, 0.0),
+            vec2(offset, 0.0),
+            vec2(-offset, -offset),
+            vec2(0.0, -offset),
+            vec2(offset, -offset)
+        );
+        float kernel[9] = float[](
+            1.0/16, 2.0/16, 1.0/16,
+            2.0/16, 4.0/16, 2.0/16,
+            1.0/16, 2.0/16, 1.0/16);
+
+        vec3 samples[9];
+        for(int i = 0; i < 9; i++){
+            samples[i] = texture(textureIn, texCoord + offsets[i]).rgb;
+        }
+        vec3 col = vec3(0.0);
+        for(int i = 0; i < 9; i++){
+            col += samples[i] * kernel[i];
+        }
+        FragColor = vec4(col, 1.0);
+    }else if(renderFlag == 6){
+        vec2 offsets[9] = vec2[](
+            vec2(-offset, offset),
+            vec2(0.0, offset),
+            vec2(offset, offset),
+            vec2(-offset, 0.0),
+            vec2(0.0, 0.0),
+            vec2(offset, 0.0),
+            vec2(-offset, -offset),
+            vec2(0.0, -offset),
+            vec2(offset, -offset)
+        );
+        float kernel[9] = float[](
+            1.0, 1.0, 1.0,
+            1.0, -8.0, 1.0,
+            1.0, 1.0, 1.0);
+
+        vec3 samples[9];
+        for(int i = 0; i < 9; i++){
+            samples[i] = texture(textureIn, texCoord + offsets[i]).rgb;
+        }
+        vec3 col = vec3(0.0);
+        for(int i = 0; i < 9; i++){
+            col += samples[i] * kernel[i];
+        }
+        FragColor = vec4(col, 1.0);
     }
 }
